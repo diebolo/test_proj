5567d2ebc94ec179d2344c40b18988c4551bcc87
Dielof van Loon <d.i.vanloon@student.tudelft.nl>
2024-03-05 15:58:09 +0100

    started calc motor check

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,109 @@
-use gilrs::{Gilrs, Axis, Button, GilrsBuilder};
+use gilrs::{Axis, Button, Gilrs, GilrsBuilder};
+use integer_sqrt::IntegerSquareRoot;
 
-// Define a struct to represent the joystick state
-#[derive(Debug)]
-pub struct Joystick {
-	Xaxis: f32,
-	Yaxis: f32,
-	abort: bool,
-	throttle: f32,
-	twist: f32,
+
+pub fn calc_motors(inp: &[i32; 4]) -> [i32; 4] {
+    // input array
+    // lift, roll, pitch, yaw
+    //      Z,      L,      M,      N
+    //ae1 -0.25	    0	    0.5	    -0.25
+    //ae2 -0.25	    -0.5	0	    0.25
+    //ae3 -0.25	    0	    -0.5	-0.25
+    //ae4 -0.25	    0.5	    0	    0.25
+
+    let b = 1; // ????
+    let d = 1; // ????
+
+    let a1 = (-25 * inp[0] + 50 * inp[2]) / b - (25 * inp[3]) / d;
+    let a2 = (-25 * inp[0] - 50 * inp[1]) / b + (25 * inp[3]) / d;
+    let a3 = (-25 * inp[0] - 50 * inp[2]) / b - (25 * inp[3]) / d;
+    let a4 = (-25 * inp[0] + 50 * inp[1]) / b + (25 * inp[3]) / d;
+
+    // let mut ae1: i32 = 0;
+    // let mut ae2: i32 = 0;
+    // let mut ae3: i32 = 0;
+    // let mut ae4: i32 = 0;
+    
+    // if a1 > 0 {
+    //     ae1 = a1.integer_sqrt();
+    // }
+    // if a2 > 0 {
+    //     ae2 = a2.integer_sqrt();
+    // }
+    // if a3 > 0 {
+    //     ae3 = a3.integer_sqrt();
+    // }
+    // if a4 > 0 {
+    //     ae4 = a4.integer_sqrt();
+    // }
+
+    // [u16::try_from(ae1).ok().unwrap(), u16::try_from(ae2).ok().unwrap(), u16::try_from(ae3).ok().unwrap(), u16::try_from(ae4).ok().unwrap()]
+    [a1, a2, a3, a4]
 }
 
-// Function to read joystick input and return the joystick state
-pub fn read_joystick(gilrs: &mut Gilrs) -> Joystick {
-	// Process all events from the gilrs library
-	while let Some(event) = gilrs.next_event() {
-		// Handle connected events
-		match event.event {
-			gilrs::EventType::Connected => {
-				println!("Gamepad connected: {:?}", event.id);
-			}
-			gilrs::EventType::Disconnected => {
-				println!("Gamepad disconnected: {:?}", event.id);
-			}
-			_ => {}
+
+
+
+#[derive(Debug)]
+pub struct Joystick{
+    Xaxis:f32,
+    Yaxis:f32,
+    abort:bool,
+    pub throttle:f32,
+    twist:f32,
+}
+
+pub struct DataBuff{
+    // x:[f32],
+    // y:[f32],
+}
+
+
+impl Joystick {
+	pub fn to_yprt(&self) -> (i32, i32, i32, i32) {
+		// Convert joystick input to yaw, pitch, and roll
+		// The joystick input is in the range [-1.0, 1.0]
+		// with max joystick roll and pitch are pi/4 radians
+		// yaw is max 0.01 radians per sec
+		let x = self.Xaxis;
+		let y = self.Yaxis;
+		let z = self.twist;
+		let roll = (x * 785.0) as i32;
+		let pitch = (y * 785.0) as i32;
+		let yaw = (z * 10.0) as i32;
+		let mut throttle = 0;
+		if self.throttle > 0.01 {
+			throttle = (self.throttle * 1000.0) as i32;
 		}
+
+		(yaw, pitch, roll, throttle)
 	}
+}
+
+
+pub fn read_joystick(gilrs: &mut Gilrs) -> Option<Joystick> {
+	// Process all events from the gilrs library
+	while let Some(_event) = gilrs.next_event() {};
 
 	// Initialize the joystick state
-	let mut joystick = Joystick {
-		Xaxis: 0.0,
-		Yaxis: 0.0,
-		abort: false,
-		throttle: 0.0,
-		twist: 0.0,
-	};
+	let mut joystick = None;
 
 	// Iterate over all connected gamepads
 	for (_id, gamepad) in gilrs.gamepads() {
 		// Update the joystick state based on gamepad input
-		//println!("{:?}", gamepad.state());
-		joystick = Joystick {
+		joystick = Some(Joystick {
 			Xaxis: gamepad.value(Axis::LeftStickX),
 			Yaxis: gamepad.value(Axis::LeftStickY),
 			abort: gamepad.is_pressed(Button::South),
-			throttle: gamepad.value(Axis::RightStickY) as f32 / 2.0 + 0.5,
+			throttle: ((gamepad.value(Axis::RightStickY) / 2.0) + 0.5) as f32,
 			twist: gamepad.value(Axis::RightStickX),
-		};
+		});
 	}
 
 	// Return the joystick state
 	return joystick;
 }
+
 
 fn main() {
 	// Create a new instance of the Gilrs library
@@ -65,7 +118,16 @@ fn main() {
 	// Continuously read joystick input and print the joystick state
 	loop {
 		let js = read_joystick(&mut gilrs);
-		println!("{:?}", js);
+		match js {
+			Some(js) => {
+				println!("{:?}", js);
+				let (y, p, r, t) = js.to_yprt();
+				let motors = calc_motors(&[t, r, p, y]);
+				println!("{:?}", motors);
+
+			},
+			None => println!("No joystick connected"),
+		}
 		std::thread::sleep(delay);
 	}
 }
